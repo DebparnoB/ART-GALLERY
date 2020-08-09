d3e9063db8a01ee974acd09d39a94b09409e47e2
Debparna <biswas.debparna.25@gmail.com>
2020-08-09 17:50:21 +0530

    Sign Up dialog

--- ArtGallery_UI/src/app/log-up-dialog/log-up-dialog.component.ts
+++ ArtGallery_UI/src/app/log-up-dialog/log-up-dialog.component.ts
@@ -9,34 +9,79 @@ import { forbiddenNameValidator } from './forbidden-name.directive';
 })
 export class LogUpDialogComponent implements OnInit {
  
-  hero = { first_name: 'Dr.',last_name: ''};
+  user = { first_name: '',middle_name:'',last_name: '',country: '',state: '',address: '',pin_code: '',city: '',email: '', cont_number:''};
 
-  heroForm: FormGroup;
+  signUpForm: FormGroup;
 
   login_userName: string = "";
   login_password: string = "";
 
-  page: boolean[] = [true, false, false, false];
+  page: boolean[] = [true, false, false, false, false];
   page_login: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
-    this.heroForm = new FormGroup({
-      first_name: new FormControl(this.hero.first_name, [
+    this.signUpForm = new FormGroup({
+      //page1
+      first_name: new FormControl(this.user.first_name, [
         Validators.required,
-        Validators.minLength(4),
-        forbiddenNameValidator(/bob/i)
+        Validators.pattern('[A-Za-z]*'),
+        //Validators.minLength(4),
+        //forbiddenNameValidator(/bob/i)
       ]),
-      last_name: new FormControl(this.hero.last_name, [
+      middle_name: new FormControl(this.user.middle_name,[
+        Validators.pattern('[A-Za-z]*')
+      ]),
+      last_name: new FormControl(this.user.last_name, [
         Validators.required,
-        Validators.minLength(4),
-        forbiddenNameValidator(/lost/i)
+        Validators.pattern('[A-Za-z]*')
+      ]),  
+      //page2    
+      country: new FormControl(this.user.country, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern('[A-Za-z]*')
       ]),
+      state: new FormControl(this.user.state, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern('[A-Za-z]*')
+      ]),
+      address: new FormControl(this.user.address, [
+        Validators.required
+      ]),
+      city: new FormControl(this.user.city, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern('[A-Za-z]*')
+      ]),
+      pin_code: new FormControl(this.user.pin_code, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern('[0-9]*')
+      ]),      
+      email: new FormControl(this.user.email, [
+        Validators.required,
+        Validators.pattern('[A-Za-z]*')
+      ]),      
+      cont_number: new FormControl(this.user.cont_number, [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.pattern('[0-9]*')
+      ])  
     }); // <-- add custom validator at the FormGroup level
   }
 
-  get first_name() { return this.heroForm.get('first_name'); }
-  get last_name() { return this.heroForm.get('last_name'); }
+  get first_name() { return this.signUpForm.get('first_name'); }
+  get middle_name() { return this.signUpForm.get('middle_name'); }
+  get last_name() { return this.signUpForm.get('last_name'); }
+  get country() { return this.signUpForm.get('country'); }
+  get state() { return this.signUpForm.get('state'); }
+  get address() { return this.signUpForm.get('address'); }
+  get city() { return this.signUpForm.get('city'); }
+  get pin_code() { return this.signUpForm.get('pin_code'); }
+  get email() { return this.signUpForm.get('email'); }
+  get cont_number() { return this.signUpForm.get('cont_number'); }
 
   traverse(index: number, direction: number){
     this.page[index] = false;
